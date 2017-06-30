cc11a9d4fe9beeb10ce69ee38440ece3fa75009b
Bretton Wade <bretton.wade@gmail.com>
2017-06-30 19:52:29 -0400

    a little bit of abstraction on the display of specifications

--- website/js/bag-service-base.js
+++ website/js/bag-service-base.js
@@ -40,6 +40,102 @@ let ServiceBase = function () {
         request.send ();
     };
 
+    _.displaySpecification = function (specification) {
+        // start with an empty build
+        let innerHTML = "";
+
+        if ("description" in specification) {
+            innerHTML += block ("h2", {}, "Description") + div ("description-div", specification.description);
+        }
+
+        if ("events" in specification) {
+            innerHTML += block ("h2", {}, "Events");
+            let events = specification.events;
+            let eventNames = Object.keys (events);
+            let eventsHTML = "";
+            for (let eventName of eventNames) {
+                let event = events[eventName];
+                let eventHTML = "";
+
+                // if there is an example
+                if ("example" in event) {
+                    let url = "api?event=" + eventName;
+                    let example = event.example;
+                    let exampleKeys = Object.keys (example).sort ();
+                    for (let exampleKey of exampleKeys) {
+                        url += "&" + exampleKey + "=" + example[exampleKey];
+                    }
+                    //eventHTML = a ("try-it", url, " (" + url.replace (/&/g, "&amp;") + ")");
+                    eventHTML = a ("try-it", url, "[example]");
+                }
+                eventHTML = div ("event-name", eventName + eventHTML);
+
+                // if there is a description
+                if ("description" in event) {
+                    eventHTML += div ("event-description", event.description);
+                }
+
+                let odd;
+                let evenOdd = function (title, object) {
+                    odd = true;
+                    let names = Object.keys (object);
+                    if (names.length > 0) {
+                        eventHTML += div ("even-odd-title", title);
+                        for (let name of names) {
+                            let element = object[name];
+                            let required = ("required" in element) ? element.required : false;
+                            eventHTML += div ("even-odd-div" + (odd ? " odd" : ""),
+                                div ("even-odd-name", name) +
+                                div ("even-odd-required", required ? "REQUIRED" : "OPTIONAL") +
+                                div ("even-odd-description", element.description));
+                            odd = !odd;
+                        }
+                    }
+                    return odd;
+                };
+
+                if ("parameters" in event) {
+                    evenOdd ("Parameters:", event.parameters);
+                }
+
+                if (("strict" in event) && (event.strict == "false")) {
+                    eventHTML += div ("even-odd-div" + (odd ? " odd" : ""),
+                        div ("even-odd-name", "(any)") +
+                        div ("even-odd-required", "OPTIONAL") +
+                        div ("even-odd-description", "Event allows unspecified parameters."));
+                }
+
+                if ("returns" in event) {
+                    let returns = event.returns;
+                    // return specification might be an array, indicating this event returns an
+                    // array of something
+                    if (Array.isArray(returns)) {
+                        // return specification might be an empty array, or an array with a single
+                        // proto object
+                        if (returns.length > 0) {
+                            evenOdd("Returns Array of:", returns[0]);
+                        } else {
+                            eventHTML += div ("even-odd-title", "Returns: Array");
+                        }
+                    } else {
+                        evenOdd("Returns:", returns);
+                    }
+                }
+
+                eventsHTML += div ("event-div", eventHTML);
+            }
+            innerHTML += div("events-div", eventsHTML);
+        }
+
+        if ("name" in specification) {
+            document.title = specification.name;
+            innerHTML = block ("h1", {}, specification.name) + div("container-div", innerHTML);
+        }
+        innerHTML += div ("content-center footer", "Built with " + a ("footer-link", "http://bag-service-base.brettonw.com", "brettonw/BagServiceBase"));
+
+        return innerHTML;
+    };
+
     // a little black raincloud, of course
     _.display = function (displayInDivId, inputUrl) {
         let url = (typeof (inputUrl) !== "undefined") ? inputUrl : "api?event=help";
@@ -47,99 +143,7 @@ let ServiceBase = function () {
             // if we retrieved the api.json from the service base, get the actual response
             if (typeof (inputUrl) === "undefined") { db = db.response; }
 
-            // start with an empty build
-            let innerHTML = "";
-
-            if ("description" in db) {
-                innerHTML += block ("h2", {}, "Description") + div ("description-div", db.description);
-            }
-
-            if ("events" in db) {
-                innerHTML += block ("h2", {}, "Events");
-                let events = db.events;
-                let eventNames = Object.keys (events);
-                let eventsHTML = "";
-                for (let eventName of eventNames) {
-                    let event = events[eventName];
-                    let eventHTML = "";
-
-                    // if there is an example
-                    if ("example" in event) {
-                        let url = "api?event=" + eventName;
-                        let example = event.example;
-                        let exampleKeys = Object.keys (example).sort ();
-                        for (let exampleKey of exampleKeys) {
-                            url += "&" + exampleKey + "=" + example[exampleKey];
-                        }
-                        //eventHTML = a ("try-it", url, " (" + url.replace (/&/g, "&amp;") + ")");
-                        eventHTML = a ("try-it", url, "[example]");
-                    }
-                    eventHTML = div ("event-name", eventName + eventHTML);
-
-                    // if there is a description
-                    if ("description" in event) {
-                        eventHTML += div ("event-description", event.description);
-                    }
-
-                    let odd;
-                    let evenOdd = function (title, object) {
-                        odd = true;
-                        let names = Object.keys (object);
-                        if (names.length > 0) {
-                            eventHTML += div ("even-odd-title", title);
-                            for (let name of names) {
-                                let element = object[name];
-                                let required = ("required" in element) ? element.required : false;
-                                eventHTML += div ("even-odd-div" + (odd ? " odd" : ""),
-                                    div ("even-odd-name", name) +
-                                    div ("even-odd-required", required ? "REQUIRED" : "OPTIONAL") +
-                                    div ("even-odd-description", element.description));
-                                odd = !odd;
-                            }
-                        }
-                        return odd;
-                    };
-
-                    if ("parameters" in event) {
-                        evenOdd ("Parameters:", event.parameters);
-                    }
-
-                    if (("strict" in event) && (event.strict == "false")) {
-                        eventHTML += div ("even-odd-div" + (odd ? " odd" : ""),
-                            div ("even-odd-name", "(any)") +
-                            div ("even-odd-required", "OPTIONAL") +
-                            div ("even-odd-description", "Event allows unspecified parameters."));
-                    }
-
-                    if ("returns" in event) {
-                        let returns = event.returns;
-                        // return specification might be an array, indicating this event returns an
-                        // array of something
-                        if (Array.isArray(returns)) {
-                            // return specification might be an empty array, or an array with a single
-                            // proto object
-                            if (returns.length > 0) {
-                                evenOdd("Returns Array of:", returns[0]);
-                            } else {
-                                eventHTML += div ("even-odd-title", "Returns: Array");
-                            }
-                        } else {
-                            evenOdd("Returns:", returns);
-                        }
-                    }
-
-                    eventsHTML += div ("event-div", eventHTML);
-                }
-                innerHTML += div("events-div", eventsHTML);
-            }
-
-            if ("name" in db) {
-                document.title = db.name;
-                innerHTML = block ("h1", {}, db.name) + div("container-div", innerHTML);
-            }
-            innerHTML += div ("content-center footer", "Built with " + a ("footer-link", "http://bag-service-base.brettonw.com", "brettonw/BagServiceBase"));
-
-            document.getElementById(displayInDivId).innerHTML = innerHTML;
+            document.getElementById(displayInDivId).innerHTML = ServiceBase.displaySpecification (db);
         });
     };
 
@@ -180,16 +184,17 @@ let ServiceBase = function () {
 
         // get the api
         let url = ((typeof (apiSource) === "undefined") || (apiSource === "")) ? (baseUrl + "/api?event=help") : apiSource;
-        _.get (url, function (db) {
+        _.get (url, function (response) {
             // if we retrieved the api.json from the service base, get the actual response
-            if (typeof (apiSource) === "undefined") { db = db.response; }
+            if (typeof (apiSource) === "undefined") { response = response.response; }
 
             // start with an empty build
             let api = Object.create (null);
+            api.specification = response;
 
             // check that we got a response with events
-            if ("events" in db) {
-                let events = db.events;
+            if ("events" in response) {
+                let events = response.events;
                 let eventNames = Object.keys (events);
                 for (let eventName of eventNames) {
                     let event = events[eventName];
